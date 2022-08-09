cb60e5f8876b93165a9d9041177f505bf35ebf80
Matthew Coplan <mcoplanwork@gmail.com>
2022-08-09 19:14:03 -0400

    updated ec2 location for backend

--- src/remote/e-commerce-api/eCommerceClient.ts
+++ src/remote/e-commerce-api/eCommerceClient.ts
@@ -4,10 +4,10 @@ import axios from 'axios';
 // Very simple, but it also doesn't give us much abstraction
 const eCommerceClient = axios.create({
   withCredentials: true,
-  baseURL: 'http://localhost:8080',
+  baseURL: 'http://ec2-3-80-76-113.compute-1.amazonaws.com:8080',
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'http://localhost:3000',
+    'Access-Control-Allow-Origin': 'http://ec2-3-80-76-113.compute-1.amazonaws.com:3000',
   },
 });
 
